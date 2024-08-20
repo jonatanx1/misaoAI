868e46309bf69400dee959524ec0ca23d3fc2d64
ak-jonatan <129184757+jonatanx1@users.noreply.github.com>
2024-08-20 08:35:41 -0300

    gtrtwr

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const elemento = document.querySelector(".teste");
 const titulo = document.querySelector(".teste");
 
 titulo.textContent = "olá mundo";
+
+
+let contador = 1 ;
+
+let contador = [1,2,5,8,9];
+
+let perguntas = {
+
+    
