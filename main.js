c0f6259236ff9ed2529f46e109a2bd623fd94279
ak-jonatan <129184757+jonatanx1@users.noreply.github.com>
2024-08-27 08:39:22 -0300

    thytyhyt

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const caixapergunta = document.querySelector(".caixa-perguntas");
 const caixaalternativas = document.querySelector(".caixa-alternativas");
 const caixaresultado = document.querySelector(".caixa-resultado");
 const textoresultado = document.querySelector(".texto-resultado");
+
+let 
+let 
+let
+let
